5355542c5ae19a3120088d69b1f694abb2dfc7d1
Jakub Grzesiowski <42080179+kubaxent@users.noreply.github.com>
2020-05-18 00:17:33 +0200

    added ASP.NET project

---
+++ ASPWebApplication/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Informacje ogólne o zestawie zależą od poniższego 
+' zestawu atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' związane z zestawem.
+
+' Przejrzyj wartości atrybutów zestawu
+<Assembly: AssemblyTitle("ASPWebApplication")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ASPWebApplication")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Poniższy identyfikator GUID odpowiada atrybutowi ID biblioteki typów, jeśli ten projekt jest uwidaczniany w modelu COM
+<Assembly: Guid("87ea77a7-5299-4f3c-9838-a735159d109a")>
+
+' Informacje o wersji zestawu obejmują następujące cztery wartości:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Możesz określić wszystkie te wartości lub użyć wartości domyślnych numerów kompilacji i poprawki, 
+' stosując znak „*”, jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
